7e78ade108d8122e7527c23ca62fd8efcdbeca08
iBatistic <ivan.batistic2@gmail.com>
2026-04-02 22:44:36 +0200

    Refactor fix for cantilever and cooksMembrane

--- papers/JFNK_highOrder/linearElastic/cantilever/analyticalCantilever/analyticalCantileverTractionFvPatchVectorField.C
+++ papers/JFNK_highOrder/linearElastic/cantilever/analyticalCantilever/analyticalCantileverTractionFvPatchVectorField.C
@@ -157,7 +157,7 @@ analyticalCantileverTractionFvPatchVectorField::evaluateQuadrature
 
     // faceQuadPoints is list for the  whole mesh
     const CompactListList<point>& faceQuadPoints =
-        solMod.MLSQuadrature().faceQuadPoints();
+        solMod.displacementMLS().quadrature().faceQuadPoints();
 
     // faceQuadPoints is list for whole mesh.
     labelList nQpPerFace(this->size(), 0);
